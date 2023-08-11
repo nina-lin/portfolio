9f8b9206d345e309c4b6aa950fa71751aa392be1
Nina Lin <37187561+nina-lin@users.noreply.github.com>
2023-08-11 15:27:55 -0400

    Add files via upload

--- js/nl.js
+++ js/nl.js
@@ -1,37 +1,46 @@
-let sheetsURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQY96sqjZNA0SSiTf5y0bmlQzr7ndM10_iUKIUWUkxVLdt_jKgjwLaLll6Lmj2ujLZH6LnDePVvMWkD/pub?gid=0&single=true&output=csv';
+(function(){
+  let counter;
 
-var counter = 0;
-var data;
+  let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQY96sqjZNA0SSiTf5y0bmlQzr7ndM10_iUKIUWUkxVLdt_jKgjwLaLll6Lmj2ujLZH6LnDePVvMWkD/pub?gid=0&single=true&output=csv';
 
-function init() {
-  Papa.parse(sheetsURL, {
-    download: true,
-    header: true,
-    complete: config
-  });
+  //parser
+  function parseCSV(url) {
+    return new Promise((resolve, reject) => {
+      Papa.parse(url, {
+        download: true, //for remote files
+        header: true,
+        error: reject,
+        complete: resolve
+      });
+    });
+  }
 
-  $('.filter').each(function(i) {
-    $(this).addClass('f' + i)
-  })
+  //if multiple datasets
+  // let urls = ['url.csv', 'url2.csv', ..., 'url-n.csv']
 
-  $('.filter')
-    .last().css('text-decoration', 'none').append('.')
-};
+  // let promises = urls.map(url => parseCSV(url))
+
+  // Promise.all(promises)
+  
+  Promise.all([parseCSV(url)])
+    .then(results => {
+      data = results[0].data;
+
+      config()
+    })
+    .catch(error => {
+      console.error('Error:', error)
+    });
 
 
-function config(results) {
-  data = results.data;
+  function config() {
+    data.forEach(function(d, i){
+      let main = document.getElementById('mainGrid')
 
-  data.forEach(function(d, i){
+      let slats = `<div id="s`+ i +`" class="slats"><a target="_blank" href="${d.link} "><img alt="${d.name}" src="${d.media}" /><div class="projectLabel"><span class="dateline">${d.date}</span><span class="pName"> - ${d.name}</span></div></a></div>`
 
-  $('#mainGrid').append('<div id="s'+i+'" class="slats"><a target="_blank" href="' + d.link + '"><img alt="'+ d.name +'" src="'+ d.media +'" /><div class="projectLabel"><span class="dateline">'+ d.date +'</span><span class="pName"> - '+ d.name +'</span></div></a></div>')
+      main.insertAdjacentHTML('beforeend', slats)
+    })
+  }
 
-
-
-  })
-
-};
-
-$(document).ready(function(){
-	init();
-});
+})();
