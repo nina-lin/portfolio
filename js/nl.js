bec72867a4cd4fe8be9246df3be546488e306b59
Nina Lin <37187561+nina-lin@users.noreply.github.com>
2024-05-30 23:37:23 -0400

    Add files via upload

--- js/nl.js
+++ js/nl.js
@@ -1,6 +1,6 @@
-(function(){
-  let counter;
+//Masonry layout adapted from Andy Barefoot's CSS Grid tutorial https://medium.com/@andybarefoot/a-masonry-style-layout-using-css-grid-8c663d355ebb
 
+(function(){
   let url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQY96sqjZNA0SSiTf5y0bmlQzr7ndM10_iUKIUWUkxVLdt_jKgjwLaLll6Lmj2ujLZH6LnDePVvMWkD/pub?gid=0&single=true&output=csv';
 
   //parser
@@ -14,19 +14,15 @@
       });
     });
   }
-
-  //if multiple datasets
-  // let urls = ['url.csv', 'url2.csv', ..., 'url-n.csv']
-
-  // let promises = urls.map(url => parseCSV(url))
-
-  // Promise.all(promises)
   
   Promise.all([parseCSV(url)])
     .then(results => {
       data = results[0].data;
 
-      config()
+      config(); 
+
+      window.onload = setTimeout(resizeAllItems, 100)
+      window.addEventListener('resize', resizeAllItems);
     })
     .catch(error => {
       console.error('Error:', error)
@@ -34,12 +30,44 @@
 
 
   function config() {
-    data.forEach(function(d, i){
-      let main = document.getElementById('mainGrid')
+    data.forEach((d, i) => {
+      let grid = document.getElementById('mainGrid')
 
-      let slats = `<div id="s`+ i +`" class="slats"><a target="_blank" href="${d.link} "><img alt="${d.name}" src="${d.media}" /><div class="projectLabel"><span class="dateline">${d.date}</span><span class="pName"> - ${d.name}</span></div></a></div>`
+      let slat = `
+        <div id="s`+ i +`" class="slat">
+          <div class="wrapper">
+            <a target="_blank" href="${d.link} ">
+              <img alt="${d.name}" src="${d.media}" />
+              <div class="projectLabel"><span class="dateline"></span><span class="pName">${d.name}</span></div>
+            </a>
+          </div>
+        </div>
+      `
 
-      main.insertAdjacentHTML('beforeend', slats)
+      grid.insertAdjacentHTML('beforeend', slat)
+    })
+  }
+
+  function resizeGridItems(item){
+    grid = document.getElementById('mainGrid')
+    
+    rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+
+    rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'))
+
+    rowSpan = Math.ceil((item.querySelector('.wrapper').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap))
+
+    window.innerWidth < 400 ? 
+      item.style.gridRowEnd = 'span ' + (rowSpan + 1):
+      item.style.gridRowEnd = 'span ' + (rowSpan + 2);
+
+  }
+
+  function resizeAllItems(){
+    allItems = document.querySelectorAll('.slat')
+
+    allItems.forEach(item => {
+      resizeGridItems(item)
     })
   }
 
